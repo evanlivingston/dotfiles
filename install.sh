36b01822d734f57f31f13dfef33b00e6374970ad
Evan Livingston <evanlivingston@gmail.com>
2012-08-14 20:55:00 -0400

    remove ssh/config

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc gitconfig ssh/config vim vimrc"    # list of files/folders to symlink in homedir
+files="bashrc gitconfig vim vimrc"    # list of files/folders to symlink in homedir
 
 ##########
 
